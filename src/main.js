6aa82075f8fa2b263e9db138c56c984dd3ce26f6
Shawn Urban <surbanwebdev@gmail.com>
2024-01-09 14:51:26 -0500

    Cross platform

--- src/main.js
+++ src/main.js
@@ -20,14 +20,6 @@ import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
 
-/* Theme variables */
-import './theme/variables.css';
-// Above the createApp() line
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
-// Call the element loader after the platform has been bootstrapped
-defineCustomElements(window);
-
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
